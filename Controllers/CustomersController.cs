5938a1f9ddddef6dc772c0216e2256c3464a03a0
krsonu1998-tech <krsonu1998@gmail.com>
2025-11-28 01:15:48 +0530

    check

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -16,6 +16,7 @@ namespace MVC.Controllers
 
         public CustomersController(AppDbContext context)
         {
+            string name1 = "kumar";
            string name = "kumar sonu";
             _context = context;
         }
